f8fbb29e5cf95b2601600d3411d11fed56e55651
YuliiaOsadchukkk <72582368+YuliiaOsadchukkk@users.noreply.github.com>
2020-11-29 19:11:49 +0200

    Lab_6.2-r-UT

---
+++ UT/UT.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "../Lab_6.2-r/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UT
+{
+	TEST_CLASS(UT)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int mass[6] = { 1, -30, -24, 18, 9, -3 };
+			Assert::AreEqual(do_stuff(mass, 6,0,0), 9.);
+
+		}
+	};
+}
